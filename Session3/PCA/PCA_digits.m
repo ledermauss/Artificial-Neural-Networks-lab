60fc7d883632beccc2953e523049dc7c56040be8
ledermauss <ledermauss@users.noreply.github.com>
2018-05-23 23:45:33 +0200

    Added rec error and variance analysis to the report

--- Session3/PCA/PCA_digits.m
+++ Session3/PCA/PCA_digits.m
@@ -82,26 +82,26 @@ end
 figure;
 bar(eigvals);
 title("Eigenvalues scaled")
-set(gca,'FontSize', 13); 
+set(gca,'FontSize', 14); 
 
 %% Reconstruction error vs 1 - k_cumsum(var)
 figure;
 digits_error = error_by_pc(threes,1,256,5, true);
 title('Reconstruction MSE vs eigenvalues variance (Digits)')
-set(gca,'FontSize', 13);
+set(gca,'FontSize', 16);
 
 %% Random data: rec error vs 1 - k_cumsum(var)
 gauss = randn(50,500);
 g_error = error_by_pc(gauss, 1, 50, 1, true);
 title('Reconstruction MSE vs eigenvalues variance (Gaussians)')
-set(gca,'FontSize', 13);
+set(gca,'FontSize', 16);
 
 %% Regression
 % note: second col (y axis) is the MSE error, third the cumsum
 % regression: first target (x axis), later output (y axis)
-plotregression(g_error(:,3), g_error(:,2), 'Gaussians'),
-xlabel("Sum(k-eigvals)"), ylabel("Reconstruction Error")
-set(gca,'FontSize', 13);
+plotregression(digits_error(:,3), digits_error(:,2), 'Digits - Rec. Error fit'),
+xlabel("1 - Sum(k-eigvals)"), ylabel("Reconstruction Error")
+set(gca,'FontSize', 16);
 
 %% Random trace vs digit trace
 [~, ~, evals_threes] = mypca(threes', 50);
@@ -110,8 +110,18 @@ plot(1:50, evals_gauss, 'g', 1:50, evals_threes, 'b')
 legend("Random eigenvalues", "Digits eigenvalues", 'Location', 'north'),
 xlabel('Selected components'), ylabel('Scaled eigenvalues (variance)'),
 title("Digits vs Random trace"),
-set(gca, 'FontSize', 13)
+set(gca, 'FontSize', 16)
 
+%% rec error vs cumsum - digits
+figure;
+digits_cumsum = error_by_pc(threes,1,256,5, false);
+title('Reconstruction MSE vs eigenvalues variance (Digits)')
+set(gca,'FontSize', 14);
+%% rec error vs cumsum - gauss
+figure;
+gauss_cumsum = error_by_pc(gauss,2,50,2, false);
+title('Reconstruction MSE vs eigenvalues variance (Digits)')
+set(gca,'FontSize', 14);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%  Component meaning           %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
